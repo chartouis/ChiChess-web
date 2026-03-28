c86b6d7028965f72b17361610fbb4758245539f4
chartouis <erna95er@gmail.com>
2026-03-28 20:54:51 +0500

    chore: hardcode vars

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,3 @@
-// src/lib/config.ts
-import { env } from '$env/dynamic/public';
-
-export const NURL = env.PUBLIC_NURL;
-export const WSNURL = env.PUBLIC_WSNURL;
-export const REFRESH_INTERVAL = +env.PUBLIC_REFRESH_INTERVAL;
+export const NURL = 'https://yzarr.xyz';
+export const WSNURL = 'wss://yzarr.xyz';
+export const REFRESH_INTERVAL = 500000;
